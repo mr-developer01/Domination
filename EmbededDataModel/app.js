3e25acd4389e5b5f3a85e2d18a816c32726efde9
Rahul <rahulpratik1212@gmail.com>
2024-07-23 11:46:41 +0530

    embading

--- EmbededDataModel/app.js
+++ EmbededDataModel/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const userModel = require("./models/usermodel")
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -8,10 +10,20 @@ app.get("/", (req, res) => {
   res.send("Working!!!");
 });
 
-app.post("/create", (req, res) => {
-    let createdUser = userModel.create({
-        
+app.post("/create", async(req, res) => {
+    let createdUser = await userModel.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
     })
+    res.send(createdUser)
+})
+
+app.post("/:username/create/post", async(req, res) => {
+    const user = await userModel.findOne({username: req.params.username});
+    user.posts.push({content: req.body.content});
+    await user.save();
+    res.send(user);
 })
 
 app.listen("3000");
--- EmbededDataModel/models/usermodel.js
+++ EmbededDataModel/models/usermodel.js
@@ -1,7 +1,36 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/ShreeEmbededDataModel")
+mongoose.connect("mongodb://localhost:27017/ShreeEmbededDataModel");
+
+// const userSchema = mongoose.Schema({
+//     username: String,
+//     email: String,
+//     password: String,
+//     posts: [
+//         {
+//             content: String,
+//             date: {
+//                 type: Date,
+//                 default: Date.now()
+//             }
+//         }
+//     ]
+// });
+
+// or
+
+const postSchema = mongoose.Schema({
+  content: String,
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const userSchema = mongoose.Schema({
-    username: String
-})
+  username: String,
+  email: String,
+  password: String,
+  posts: [postSchema],
+});
 
+module.exports = mongoose.model("User", userSchema);
